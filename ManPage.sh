e08f687279720b70747a0aa1b7b1628714d4fbca
hectorhmx <hectorhmx.55@gmail.com>
2018-02-25 13:10:46 -0600

    Signed-off-by: hectorhmx <hectorhmx.55@gmail.com>

--- ManPage.sh
+++ ManPage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "La ayuda está en camino, este es el menú de la PrebeShell"
-while [ $option-ne 6 ]; do
+while [ $option -ne 6 ]; do
 	echo -e "Comandos disponibles:\n\t1)Games\n\t2)PrebePlayer\n\t3)DaTree\n\t4)DaDate\n\t5)DaTime\n\t6)LeExit"
 	echo -e "Por favor, eliga una opción: \n\t"
 	read -e option
