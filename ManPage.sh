80e45fe5647360c61967dac531f66ac20e64630c
SerjAlva <sergio-d.h.a@hotmail.com>
2018-02-25 07:53:04 -0600

    ManPage totalmente funcional, solo falta implementar el resto del código v:

--- ManPage.sh
+++ ManPage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "La ayuda está en camino, este es el menú de la PrebeShell"
-while [ $option-ne 6 ]; do
+while [ "$option" != "6" ]; do
+	clear
 	echo -e "Comandos disponibles:\n\t1)Games\n\t2)PrebePlayer\n\t3)DaTree\n\t4)DaDate\n\t5)DaTime\n\t6)LeExit"
 	echo -e "Por favor, eliga una opción: \n\t"
 	read -e option
@@ -11,16 +12,23 @@ while [ $option-ne 6 ]; do
 			echo -e "\n\tb) Gato"
 			echo -e "\n\tc) Hola"
 			read -e gameoption
-			echo "Su opción fue $gameoption";;
+			echo "Su opción fue $gameoption\n";;
 		2)
-			echo -e "\n y aqui pondria musica si supiera como :v";;
+			echo -e "\n y aqui pondria musica si supiera como :v\n";;
 		3)
-			echo -e "\n imaginemos que sé implementar el comando tree v:";;
+			echo -e "\n imaginemos que sé implementar el comando tree v:\n";;
 		4)
 			echo -e $ cal;;
 		5)
 			echo "";;
 		6)
-			exit;;
+			break
 	esac
+	echo -e  "¿Desea ingresar un nuevo comando?\n\n\t1)Sí\n\t2)Nel\n\t"
+	read -e elec
+	if [ "$elec" = "2" ]
+	then
+		break
+	fi
+
 done
