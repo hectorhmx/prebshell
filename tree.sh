c263799c9db4764b7121699375e1bd27aa85d766
hector <hectorhmx.55@gmail.com>
2018-03-04 09:50:14 -0600

    Comando tree detecta archivos, lee entradas vacías. Solución a un bug causado al ingresar / como parametro.

--- tree.sh
+++ tree.sh
@@ -4,14 +4,22 @@ for i in $1/*
 do
 	if [ -d $i ];then
 		cd $i
-		echo $PWD
-		arbolito $PWD
+		echo -e "\e[31m$i" ##le da color rojo a los directorios
+		arbolito $PWD	##recursion
+	elif [ -f $i ];then
+		echo -e "\e[34m$i" ## le da color azul a los directorios
 	else
-		continue
+		continue ## evita ciertos problemas que surgen al explorar los directorios
 	fi
 done
 }
-echo "dame la ruta perro"
-read -e r
-clear
-arbolito $r
+read -e a
+if [ -z $a ];then
+	arbolito $PWD
+elif [ $a="/" ];then
+	arbolito
+else
+	arbolito $a
+fi
+
+
