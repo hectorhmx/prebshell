938166f22f2f410e2dafca5a3f7e7ffc81ca7cce
hector <hectorhmx.55@gmail.com>
2018-03-03 20:01:40 -0600

    Correciones al archivo tree, agregada la función de moverse entre directorios

--- tree.sh
+++ tree.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-arbolito (){
-for i in ($1/*)
+arbolito(){
+for i in $1/*
 do
 	if [ -d $i ];then
 		cd $i
 		echo $PWD
 		arbolito $PWD
+	else
+		continue
 	fi
 done
 }
-read
-arbolito
+echo "dame la ruta perro"
+read -e r
+clear
+arbolito $r
