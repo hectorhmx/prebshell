78f163fb72b6831502f2b37d4fdf3840c6b00347
SerjAlva <sergio-d.h.a@hotmail.com>
2018-03-07 21:02:11 -0600

    Se agregaron colores a la PrebePlay

--- PrebePlay.sh
+++ PrebePlay.sh
@@ -1,81 +1,171 @@
 #!/bin/bash
 
-echo -e "		   Bienvenido a la PrebePlay"
-echo -e "Aquí podrás reproducir tu música favorita sin salir de tu terminal"
-echo -e "		     Selecciona una opción:"
-echo -e "1)Todo el contenido de una carpeta en orden alfabético"
-echo -e "2)Todo el contenido de una carpeta de modo aleatorio."
-echo -e "3)Todo el contenido de una carpeta de modo aleatorio con repeticiones"
-echo -e "4)Una sola canción"
-read -e opcion
+clear
+echo -e "\e[47;37m ----------------------------------------------------------------------------------------"
+echo -e "\e[47;37m||\e[101;37m										       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m			   Bienvenido a la PrebePlay				       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m	 Aquí podrás reproducir tu música favorita sin salir de tu terminal	       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m			     Selecciona una opción:			               \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m	 1)Todo el contenido de una carpeta en orden alfabético			       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m	 2)Todo el contenido de una carpeta de modo aleatorio.			       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m	 3)Todo el contenido de una carpeta de modo aleatorio con repeticiones	       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m	 4)Una sola canción							       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m       5)Salir								       \e[47;37m||"
+echo -e "\e[47;37m||\e[101;37m										       \e[47;37m||"
+echo -e "\e[47;37m ----------------------------------------------------------------------------------------\e[0m\n\n\t"
+read -e -p ">>"  opcion
 case $opcion in
 	1)	clear
-		echo -e "Ingresa la ruta de la carpeta que quieras escuchar:\n>>"
-		read -e ruta
+		echo -e "\e[47;37m-----------------------------------------------------------"
+		echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+		echo -e "\e[47;37m||\e[101;37m  Ingresa la ruta de la carpeta que quieras escuchar:  \e[47;37m||"
+		echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+		echo -e "\e[47;37m-----------------------------------------------------------\e[0m\n\n\t"
+		read -e -p ">>" ruta
 			cd $ruta
 			if [ $? != 0 ]; then
-				echo -e "\n\nLa ruta indicada es invalida, por favor, verificala"
-				echo -e "Presiona enter para continuar..."
+				cd
+				clear
+				echo -e "\e[47;37m---------------------------------------------------------------\e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m							       \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m	La ruta indicada es invalida, por favor, verificala    \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m	Presiona enter para continuar...		       \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m							       \e[47;37m||"
+				echo -e "\e[47;37m---------------------------------------------------------------\e[47;37m||\e[0m\n\n\t"
 				read
 			else
 				cd
 				clear
-				echo -e "Directorio encontrado, reproduciendo..."
-				echo -e "Para controlar la reprodución utilice las siguientes opciones:"
-				echo -e "\tp)Pausar.\n\ts)Detener\n\tf)Siguiente canción\n\tb)Retroceder una canción\n\tq)Salir de la PrebePlay"
+				echo -e "\e[47;37m----------------------------------------------------------------------------\e[47;37m|"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m	.............Directorio encontrado, reproduciendo.............	   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m	Para controlar la reprodución utilice las siguientes opciones:     \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m	\e[41;97mp)Pausar.\e[101;37m							   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m		\e[41;97ms)Detener.\e[101;37m						   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m			\e[41;97mf)Siguiente.\e[101;37m					   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m				\e[41;97mb)Retroceder.\e[101;37m				   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m					\e[41;97mq)Salir de la PrebePlay.\e[101;37m	   \e[47;37m||"
+				echo -e "\e[47;37m||\e[101;37m									   \e[47;37m||"
+				echo -e "\e[47;37m----------------------------------------------------------------------------\e[47;37m|\e[0m"
 				mpg123 -C --title -q $ruta/*
 			fi;;
 	2)	clear
-		echo -e "Ingresa la ruta de la carpeta que quieras escuchar:\n>>"
-                read -e ruta
+                echo -e "\e[47;37m-----------------------------------------------------------"
+		echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+                echo -e "\e[47;37m||\e[101;37m  Ingresa la ruta de la carpeta que quieras escuchar:  \e[47;37m||"
+		echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+                echo -e "\e[47;37m-----------------------------------------------------------\e[0m\n\n\t"
+                read -e -p ">>" ruta
                         cd $ruta
                         if [ $? != 0 ]; then
-                                echo -e "La ruta indicada es invalida, por favor, verificala"
-                                echo -e "Presiona enter para continuar..."
+				cd
+				clear
+                                echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  La ruta indicada es invalida, por favor, verificala    \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  Presiona enter para continuar...                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m\e[0m\n\n\t"
                                 read
 
                         else
 				clear
                                 cd
-                                echo -e "Directorio encontrado, reproduciendo..."
-                                echo -e "Para controlar la reprodución utilice las siguientes opciones:"
-                                echo -e "\tp)Pausar.\n\ts)Detener\n\tf)Siguiente canción\n\tb)Retroceder una canción\n\tq)Salir de la PrebePlay"
+                                echo -e "\e[47;37m------------------------------------------------------------------------\e[47;37m|"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  .............Directorio encontrado, reproduciendo.............     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  Para controlar la reprodución utilice las siguientes opciones:     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  \e[41;97mp)Pausar.\e[101;37m                                                          \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m          \e[41;97ms)Detener.\e[101;37m                                                 \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                  \e[41;97mf)Siguiente.\e[101;37m                                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                          \e[41;97mb)Retroceder.\e[101;37m                              \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                  \e[41;97mq)Salir de la PrebePlay.\e[101;37m           \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m------------------------------------------------------------------------\e[47;37m|\e[0m"
 				mpg123 -C --title -q -z $ruta/*
                         fi;;
 	3)	clear
-		echo -e "Ingresa la ruta de la carpeta que quieras escuchar:\n>>"
-                read -e ruta
+		echo -e "\e[47;37m-----------------------------------------------------------"
+                echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+                echo -e "\e[47;37m||\e[101;37m  Ingresa la ruta de la carpeta que quieras escuchar:  \e[47;37m||"
+                echo -e "\e[47;37m||\e[101;37m                                                       \e[47;37m||"
+                echo -e "\e[47;37m-----------------------------------------------------------\e[0m\n\n\t"
+                read -e -p ">>" ruta
                         cd $ruta
                         if [ $? != 0 ]; then
-                                echo -e "La ruta indicada es invalida, por favor, verificala"
-                                echo -e "Presiona enter para continuar..."
+				cd
+				clear
+                                echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  La ruta indicada es invalida, por favor, verificala    \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  Presiona enter para continuar...                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m\e[0m\n\n\t"
                                 read
 
                         else
 				clear
                                 cd
-                                echo -e "Directorio encontrado, reproduciendo..."
-                                echo -e "Para controlar la reprodución utilice las siguientes opciones:"
-                                echo -e "\tp)Pausar.\n\ts)Detener\n\tf)Siguiente canción\n\tb)Retroceder una canción\n\tq)Salir de la PrebePlay"
+                                echo -e "\e[47;37m------------------------------------------------------------------------\e[47;37m|"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  .............Directorio encontrado, reproduciendo.............     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  Para controlar la reprodución utilice las siguientes opciones:     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  \e[41;97mp)Pausar.\e[101;37m                                                          \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m          \e[41;97ms)Detener.\e[101;37m                                                 \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                  \e[41;97mf)Siguiente.\e[101;37m                                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                          \e[41;97mb)Retroceder.\e[101;37m                              \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                  \e[41;97mq)Salir de la PrebePlay.\e[101;37m           \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                                     \e[47;37m||"
+                                echo -e "\e[47;37m------------------------------------------------------------------------\e[47;37m|\e[0m"
 				mpg123 -C --title -q -Z $ruta/*
                         fi;;
 
 	4)	clear
-		echo -e "Ingresa la ruta y el nombre de la canción que quieras escuchar:\n>>"
-		read -e ruta
+		echo -e "\e[47;37m-----------------------------------------------------------------------------"
+                echo -e "\e[47;37m||\e[101;37m                                                                         \e[47;37m||"
+                echo -e "\e[47;37m||\e[101;37m  Ingresa la ruta y el nombre de la canción que quieras escuchar:        \e[47;37m||"
+                echo -e "\e[47;37m||\e[101;37m                                                                         \e[47;37m||"
+                echo -e "\e[47;37m-----------------------------------------------------------------------------\e[0m\n\n\t"
+		read -e -p ">>" ruta
 			cd $ruta
 			if [ $? != 0 ]; then
-				echo -e "La ruta indicada es invalida, por favor, verificala"
-                                echo -e "Presiona enter para continuar..."
+				cd
+				clear
+				echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  La ruta indicada es invalida, por favor, verificala    \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  Presiona enter para continuar...                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                         \e[47;37m||"
+                                echo -e "\e[47;37m-------------------------------------------------------------\e[47;37m\e[0m\n\n\t" 
                                 read
 
 			else
 				clear
 				cd
-				echo -e "La ruta es correcta, reproduciendo..."
-				echo -e "Para quitar la canción presiona q "
+				echo -e "\e[47;37m---------------------------------------------------------\e[47;37m"
+                                echo -e "\e[47;37m||\e[101;37m                                                     \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  	La ruta es correcta 		       	       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m  	Presiona q para salir...                       \e[47;37m||"
+                                echo -e "\e[47;37m||\e[101;37m                                                     \e[47;37m||"
+                                echo -e "\e[47;37m---------------------------------------------------------\e[47;37m\e[0m\n\n\t"
 				mpg123 -C -q --title $ruta
 			fi;;
+	5)	clear
 esac
 clear
 
