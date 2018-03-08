b9d4d7bd99d55b02116b0f750c1eaeb419e55f9a
hectorhmx <hectorhmx.55@gmail.com>
2018-03-07 22:18:21 -0600

    Implementada la correcta lectura de los argumentos, agregado comando de fecha. Se agrego la ManPage a los comandos posibles de la prebeshell. Solucionado bug de colores en el arbolito

--- principal.sh
+++ principal.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 ##realizará las capturas de la prebeshell.
-BOOL=1
-while [ $BOOL ]; ##repetición hasta que se elija salir
+clear
+echo "para conocer los comandos escribe HelpPlox"
+while [ 1 ]; ##repetición hasta que se elija salir
 do
-	read -p "$PWD//$HOSTTYPE//$HOSTNAME   " ##prompt de la prebeshell
-		if [[ "$1"=="prebeplayer" ]];then ##estructura para elegir alguna opción.
-		./prebeplayer.sh
-		elif [[ "$1"=="buscador" ]];then
-		./buscador.sh $2 $3
-		elif [[ "$1"=="salir" ]];then
-			$BOOL = 0
-		elif [[ "$1"=="arbolito" ]];then
-		./tree.sh
-		elif [[ "$1"=="juego1" ]];then
-			echo "mostrar juego 1"
-		elif [[ "$1"=="juego2" ]];then
-			echo "mostrar juego 2"
-		elif [[ "$1"=="juego3" ]];then
-			echo "mostrar juego 3"
-		elif [[ "$1"=="date" ]];then
-			mkdir holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl
-			stat -c%y holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl | echo
-			rm holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl
-		else
-			echo "comando equivocado"
-		fi
+	read -p  "$PWD//$HOSTTYPE//$HOSTNAME   " a b c ##prompt de la prebeshell
+	echo $a
+	if [[ "$a" = "prebeplayer" ]];then ##estructura para elegir alguna opción.
+		./PrebePlay.sh
+	elif [[ "$a" == "buscador" ]];then
+		./buscador.sh $b $c
+	elif [[ "$a" == "salir" ]];then
+		exit 1000
+	elif [[ "$a" == "arbolito" ]];then
+		./arbolito.sh
+	elif [[ "$a" == "juego1" ]];then
+		echo "mostrar juego 1"
+	elif [[ "$a" == "juego2" ]];then
+		echo "mostrar juego 2"
+	elif [[ "$a" == "juego3" ]];then
+		echo "mostrar juego 3"
+	elif [[ "$a" == "date" ]];then
+		mkdir holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl
+		stat -c%y holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl
+		rm -d holaholayalleguetrajepollitoyespam12342.192.168.255.255.hechobajolicenciagpl
+	elif [[ "$a" == "HelpPlox" ]];then
+		./ManPage.sh
+	else
+		echo "comando equivocado"
+	fi
 done
-exit
